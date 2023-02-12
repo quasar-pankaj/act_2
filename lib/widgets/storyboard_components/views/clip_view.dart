952c929d94fb41bc0d094d4914b0320ae7c0d676
Pankaj Agarwal <quasar.pankaj@gmail.com>
2023-02-12 17:05:09 +0530

    aded 2 more classes.

---
+++ lib/widgets/storyboard_components/views/clip_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ClipView extends StatelessWidget {
+  const ClipView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/widgets/storyboard_components/views/scene_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SceneView extends StatelessWidget {
+  const SceneView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
